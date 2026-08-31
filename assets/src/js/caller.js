22167bb4906ea71e1abf5a74c7968b6b69d2cea6
Milind More <milindmore22@gmail.com>
2026-08-31 15:40:08 +0530

    feat: improve lightbox responsiveness with dynamic dimensions and fixed positioning

--- assets/src/js/caller.js
+++ assets/src/js/caller.js
@@ -2,10 +2,35 @@
  * Frontend script for initializing colorbox on video links.
  */
 jQuery( function( $ ) {
+	// Returns width/height strings that keep 16:9 inside the viewport with gutters.
+	function responsiveDimensions() {
+		var vw = window.innerWidth;
+		var vh = window.innerHeight;
+		var gutterH = vw < 600 ? 0.96 : 0.88;
+		var gutterV = vw < 600 ? 0.90 : 0.85;
+		var w = Math.round( vw * gutterH );
+		var h = Math.round( w * ( 9 / 16 ) );
+		if ( h > vh * gutterV ) {
+			h = Math.round( vh * gutterV );
+			w = Math.round( h * ( 16 / 9 ) );
+		}
+		return { width: w + 'px', height: h + 'px' };
+	}
+
 	$( '.youtube, .vimeo' ).colorbox( {
 		iframe: true,
-		width: '80%',
-		height: '80%',
+		// evaluated fresh on each open and window resize
+		width:  function() { return responsiveDimensions().width; },
+		height: function() { return responsiveDimensions().height; },
+		onOpen: function() {
+			$( 'html' ).addClass( 'ytfancybox-open' );
+		},
+		onClosed: function() {
+			$( 'html' ).removeClass( 'ytfancybox-open' );
+		},
+		onResize: function() {
+			$.colorbox.resize( responsiveDimensions() );
+		},
 	} );
 } );
 
